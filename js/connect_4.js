538e2faee6d261b30f7ae057d4352da41fdd526d
Caleb Sonnichsen <csonnichsen186@west-mec.org>
2020-02-26 14:37:40 -0700

    Changed the code in the JS file
    I add an array

---
+++ js/connect_4.js
@@ -0,0 +1,22 @@
+var board = [
+	['_', '_', '_', '_', '_', '_', '_'],
+	['_', '_', '_', '_', '_', '_', '_'],
+	['_', '_', '_', '_', '_', '_', '_'],
+	['_', '_', '_', '_', '_', '_', '_'],
+	['_', '_', '_', '_', '_', '_', '_']
+]
+var a = true;
+var b = false;
+function color()
+{
+	for(i = 0; i < row.length; i++) {
+		if(a = a) 
+		{
+			document.getElementById("color").style.backgroundColor = "red";
+		}
+		if(b = b)
+		{
+			document.getElementById("color").style.backgroundColor = "yellow";
+		}
+	}
+}
