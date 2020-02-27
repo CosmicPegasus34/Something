efc620be15aed9f68ac55013256d09029eaedeca
kade zarinegar <kzarinegar706@west-mec.org>
2020-02-27 15:29:55 -0700

    Added colors for barriers, path, and finish and add more js
    Added red for barriers, green for path, and teal for finish, and changed and added more js

--- js/maze.js
+++ js/maze.js
@@ -5,16 +5,31 @@ var spaces = [
 	[0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0]
 ]
-var currentPosX, currentPosY;
-currentPosX =0; currentPosY=0;
+var currentPosX, currentPosY,currentID;
+currentPosX =1; currentPosY=1;
+function moves()
+{
+	for(var count=0;count<=5;count++)
+	{
+		for(var counted=0;counted <=5;counted++)
+		{
+			var maker = "r"+currentPosX+"c"+currentPosY;
+			console.log(maker);
+		  document.getElementById(maker).style.backgroundColor="green";
+		}
+	}
+}
+
 function down(event){
 	var pressed = event.key;
 	if(pressed == "s"){
-		spaces[currentPosX][currentPosY] = 0;
-		spaces[currentPosX+1][currentPosY] = 1;
-		for(var x=0; x<8; x++){
-			for(var y=0;y<7;y++){
-				console.log(spaces[x][y])
+		//spaces[currentPosX][currentPosY] = 0;
+		currentPosX +=1;
+		//spaces[currentPosX][currentPosY] = 1;
+		moves()
+		for(var x=0; x<6; x++){
+			for(var y=0;y<6;y++){
+				console.log(spaces[currentPosX][currentPosY]);
 			}
 		}
 	}
@@ -22,23 +37,27 @@ function down(event){
 function up(event){
 	var pressed = event.key;
 	if(pressed == "w"){
-		spaces[currentPosX][currentPosY] = 0;
-		spaces[currentPosX-1][currentPosY] = 1;
-		for(var x=0; x<8; x++){
-			for(var y=0;y<7;y++){
-				console.log(spaces[x][y])
+		//spaces[currentPosX][currentPosY] = 0;
+		currentPosX -=1;
+		//spaces[currentPosX][currentPosY] = 1;
+		moves()
+		for(var x=0; x<5; x++){
+			for(var y=0;y<5;y++){
+				console.log(spaces[x][y]);
 			}
 		}
 	}
 }
 function left(event){
 	var pressed = event.key;
-	if(pressed == "a"){
-		spaces[currentPosX][currentPosY] = 0;
-		spaces[currentPosX][currentPosY-1] = 1;
-		for(var x=0; x<8; x++){
-			for(var y=0;y<7;y++){
-				console.log(spaces[x][y])
+	if(pressed == "a" || ""){
+		//spaces[currentPosX][currentPosY] = 0;
+		currentPosY -=1;
+		//spaces[currentPosX][currentPosY] = 1;
+		moves()
+		for(var x=0; x<5; x++){
+			for(var y=0;y<5;y++){
+				console.log(spaces[x][y]);
 			}
 		}
 	}
@@ -46,13 +65,28 @@ function left(event){
 function right(event){
 	var pressed = event.key;
 	if(pressed == "d"){
-		spaces[currentPosX][currentPosY] = 0;
-		spaces[currentPosX][currentPosY+1] = 1;
-		for(var x=0; x<8; x++){
-			for(var y=0;y<7;y++){
-				console.log(spaces[x][y])
+		//spaces[currentPosX][currentPosY] = 0;
+		currentPosY +=1;
+		//spaces[currentPosX][currentPosY] = 1;
+		moves()
+		for(var x=0; x<5; x++){
+			for(var y=0;y<5;y++){
+				console.log(spaces[x][y]);
 			}
 		}
 	}
 }
 
+function tempted(x){
+	currentID = x.id;
+	console.log(currentID);
+}
+function barrier() {
+	for(var barrier)
+	{
+		if(i=0;i<1; i++)
+		{
+			
+		}
+	}
+}
